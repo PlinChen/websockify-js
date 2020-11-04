b8ae12d77312d458a10363cc70eaf682800ec527
Plin <zhi.chen@unity3d.com>
2020-11-04 16:13:20 +0800

    adapt to unity profiler

--- websockify/websockify.js
+++ websockify/websockify.js
@@ -16,7 +16,7 @@ var argv = require('optimist').argv,
     url = require('url'),
     path = require('path'),
     fs = require('fs'),
-    mime = require('mime-types'),
+    policyfile = require('policyfile'),
 
     Buffer = require('buffer').Buffer,
     WebSocketServer = require('ws').Server,
@@ -25,7 +25,8 @@ var argv = require('optimist').argv,
     source_host, source_port, target_host, target_port,
     web_path = null;
 
-
+    var PLAYER_MESSAGE_MAGIC_NUMBER = 0x67A54E8F;
+    var messageHeaderSize = 24;
 // Handle new WebSocket client
 new_client = function(client, req) {
     var clientAddr = client._socket.remoteAddress, log;
@@ -38,58 +39,88 @@ new_client = function(client, req) {
     log('WebSocket connection');
     log('Version ' + client.protocolVersion + ', subprotocol: ' + client.protocol);
 
-    if (argv.record) {
-      var rs = fs.createWriteStream(argv.record + '/' + new Date().toISOString().replace(/:/g, "_"));
-      rs.write('var VNC_frame_data = [\n');
-    } else {
-      var rs = null;
-    }
 
     var target = net.createConnection(target_port,target_host, function() {
         log('connected to target');
+        target.message = {
+          buffer: new Buffer(messageHeaderSize),
+          read: 0,
+          size: 0,
+        }
     });
     target.on('data', function(data) {
         //log("sending message: " + data);
-
-        if (rs) {
-          var tdelta = Math.floor(new Date().getTime()) - start_time;
-          var rsdata = '\'{' + tdelta + '{' + decodeBuffer(data) + '\',\n';
-          rs.write(rsdata);
-        }
-
         try {
-            client.send(data);
-        } catch(e) {
-            log("Client closed, cleaning up target");
-            target.end();
-        }
+          if (client.protocol === 'base64') {
+              client.send(new Buffer(data).toString('base64'));
+          } else {
+              // The following code splits the incoming data on the message boundaries and sends it to the websocket.
+              // Incomplete messages are stored in the buffer.
+              for(var position = 0; position < data.length;) {
+                  // target.message.size is the total size of the message, including the header and the body.
+                  // It is set only after the message header has been received (when set, it is always non-zero, as it includes the header size)
+                  if (!target.message.size) {
+                      // receiving the message header
+                      var read = Math.min(data.length - position, messageHeaderSize - target.message.read);
+                      if (read) {
+                          data.copy(target.message.buffer, target.message.read, position, position + read);
+                          position += read;
+                          target.message.read += read;
+                      }                  
+                      if (target.message.read == messageHeaderSize) {
+                          // the message header has been received            
+                          if (target.message.buffer.readUInt32LE(0) != PLAYER_MESSAGE_MAGIC_NUMBER) { // m_MagicNumber is at offset 0
+                              client.destroy();
+                              throw 'PLAYER_MESSAGE_MAGIC_NUMBER mismatch';
+                          }
+                          target.message.size = messageHeaderSize + target.message.buffer.readUInt32LE(20); // m_Size is at offset 20
+                          if (target.message.size > target.message.buffer.length) {
+                              // reallocate the buffer if the message does not fit
+                              var buffer = target.message.buffer;
+                              target.message.buffer = new Buffer(target.message.size);
+                              buffer.copy(target.message.buffer, 0, 0, target.message.read);
+                          }
+                      }
+                  }
+                  if (target.message.size) {
+                      // receiving the message body
+                      var read = Math.min(data.length - position, target.message.size - target.message.read)
+                      if (read) {
+                          data.copy(target.message.buffer, target.message.read, position, position + read);
+                          position += read;
+                          target.message.read += read;
+                      }
+                      if (target.message.read == target.message.size) {
+                          // the message body has been received
+                          client.send(target.message.buffer.slice(0, target.message.size), {binary: true});
+                          target.message.read = target.message.size = 0;
+                      }
+                  }
+              }
+          }
+      } catch(e) {
+          log("Client closed, cleaning up target");
+          target.end();
+      }
     });
     target.on('end', function() {
         log('target disconnected');
         client.close();
-        if (rs) {
-          rs.end('\'EOF\'];\n');
-        }
     });
     target.on('error', function() {
         log('target connection error');
         target.end();
         client.close();
-        if (rs) {
-          rs.end('\'EOF\'];\n');
-        }
     });
 
     client.on('message', function(msg) {
         //log('got message: ' + msg);
 
-        if (rs) {
-          var rdelta = Math.floor(new Date().getTime()) - start_time;
-          var rsdata = ('\'}' + rdelta + '}' + decodeBuffer(msg) + '\',\n');
-          rs.write(rsdata);
-        }
-
-        target.write(msg);
+        if (client.protocol === 'base64') {
+          target.write(new Buffer(msg, 'base64'));
+      } else {
+          target.write(msg,'binary');
+      }
     });
     client.on('close', function(code, reason) {
         log('WebSocket client disconnected: ' + code + ' [' + reason + ']');
@@ -159,13 +190,7 @@ http_request = function (request, response) {
                 return http_error(response, 500, err);
             }
 
-            var headers = {};
-            var contentType = mime.contentType(path.extname(filename));
-            if (contentType !== false) {
-              headers['Content-Type'] = contentType;
-            }
-
-            response.writeHead(200, headers);
+            response.writeHead(200);
             response.write(file, "binary");
             response.end();
         });
